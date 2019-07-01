476cbea88ecf98e64433e93404fb3e0a68134bc1
LittleBird <ilms49898723@gmail.com>
2019-07-01 10:10:03 +0900

    merge macOS & Ubuntu

--- ubuntu/install_dots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-cp .[^.]* ${HOME}/
-
-mkdir -p ${HOME}/.config/nvim/
-cp ./init.vim ${HOME}/.config/nvim/
--- ubuntu/install_vim_plug.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-curl -fLo "~/.vim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-
-curl -fLo "~/.local/share/nvim/site/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
