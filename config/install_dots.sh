fd388224dafad93605df8e7636ddc7527bd8acb8
LittleBird <ilms49898723@gmail.com>
2019-07-11 05:09:15 +0900

    update for neovim

--- config/install_dots.sh
+++ config/install_dots.sh
@@ -6,3 +6,6 @@ cp .[^.]* ${HOME}/
 
 mkdir -p ${HOME}/.config/nvim/
 cp ./init.vim ${HOME}/.config/nvim/
+
+mkdir -p ${HOME}/.config/nvim/after/syntax
+cp ./nvim/after/syntax/* ${HOME}/.config/nvim/after/syntax/
