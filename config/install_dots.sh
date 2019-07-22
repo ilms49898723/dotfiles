893d260e3aa9c95f51450f7db19c5f96cacb8c04
LittleBird <ilms49898723@gmail.com>
2019-07-22 22:27:21 +0900

    update tmux settings

--- config/install_dots.sh
+++ config/install_dots.sh
@@ -7,5 +7,8 @@ cp .[^.]* ${HOME}/
 mkdir -p ${HOME}/.config/nvim/
 cp ./init.vim ${HOME}/.config/nvim/
 
-mkdir -p ${HOME}/.config/nvim/after/syntax
+mkdir -p ${HOME}/.config/nvim/after/syntax/
 cp ./nvim/after/syntax/* ${HOME}/.config/nvim/after/syntax/
+
+mkdir -p ${HOME}/.tmux.plugin/
+cp -r ./tmux-plugin/* ${HOME}/.tmux.plugin/
