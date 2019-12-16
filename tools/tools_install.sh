0235fa51d6ea6932144fd186e9bda9f2d2167b27
LittleBird <ilms49898723@gmail.com>
2019-12-16 17:06:26 +0900

    update install scripts

---
+++ tools/tools_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt install build-essential
+sudo apt install cmake
+sudo apt install cscope
+sudo apt install curl
+sudo apt install git
+sudo apt install libtool-bin
+sudo apt install make
+sudo apt install ninja-build
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install tmux
+sudo apt install wget
