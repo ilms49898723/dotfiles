582fbfed34429bbc4277d291aae105c6bfd5bb81
LittleBird <ilms49898723@gmail.com>
2019-12-21 13:39:29 +0900

    update git config script

--- git_config.sh
+++ git_config.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+# Global ignores
 git config --global core.excludesfile ~/.gitignore_global
+
+# Conflict diff style
+git config --global merge.conflictstyle diff3
+
+# Merge tool
+git config --global mergetool.keepBackup false
+
+# Setting for vim-mergetool
+git config --global merge.tool nvimMerge
+git config --global mergetool.nvimMerge.cmd 'nvim -c "MergetoolStart" "$MERGED" "$BASE" "$LOCAL" "$REMOTE"'
+git config --global mergetool.nvimMerge.trustExitCode true
